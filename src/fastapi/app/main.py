c0892a9ad9db09af9307c400b5c19e6652530c52
guilherme <vinespedro55@gmail.com>
2025-01-03 15:34:08 -0300

    fix: cors

--- src/fastapi/app/main.py
+++ src/fastapi/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.fastapi.routers.create_pix_qrcode import router as create_pix_qrcode_router
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(create_pix_qrcode_router)
